69d7092a05bffe61c1f00b5dbb23bba68cd298c7
EdgarDegas <sym9688@outlook.com>
2018-07-09 21:52:34 +0800

    Complete JSON write

--- cat.py
+++ cat.py
@@ -5,17 +5,19 @@ notebook1 = open('1.ipynb')
 notebook1_str = notebook1.read()
 
 notebook1_json = json.loads(notebook1_str)
-print(notebook1_json)
-
-
 cells1 = notebook1_json['cells']
 
 notebook2 = open('2.ipynb')
 notebook2_str = notebook2.read()
 notebook2_json = json.loads(notebook2_str)
-print(notebook1_json)
 
 cells2 = notebook2_json['cells']
 
+
 # cat two notebooks
 target_cells = cells1 + cells2
+
+target_json = json.dumps(target_cells)
+
+target = open('target_notebook.ipynb', 'w')
+target.write(target_json)
