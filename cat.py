4cceab1122df1c8f0e8836dbfcc46beaa251796d
iMoeNya <sym9688@outlook.com>
2018-07-13 21:38:45 +0800

    Read Argument

--- cat.py
+++ cat.py
@@ -1,13 +1,20 @@
 import json
+import sys
+
+# accept argument
+notebook_path1 = sys.argv[1] 
+notebook_path2 = sys.argv[2]
+
+
 
 # read notebooks
-notebook1 = open('1.ipynb')
+notebook1 = open(notebook_path1)
 notebook1_str = notebook1.read()
 
 notebook1_json = json.loads(notebook1_str)
 cells1 = notebook1_json['cells']
 
-notebook2 = open('2.ipynb')
+notebook2 = open(notebook_path2)
 notebook2_str = notebook2.read()
 notebook2_json = json.loads(notebook2_str)
 
