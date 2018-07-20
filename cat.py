0c0d0c06f12bc3b848312d16ad0d3c862bb388cf
iMoeNya <sym9688@outlook.com>
2018-07-20 14:53:12 +0800

    完成

--- cat.py
+++ cat.py
@@ -14,22 +14,18 @@ target_notebook_dct = {}
 
 # read notebooks
 notebook_lst = []
-
-# read notebook path list
 for path in notebook_path_lst:
     notebook_lst.append(Notebook(path))
 
 # read cells from notebooks
 cells_lst = []
 for notebook in notebook_lst:
-    cells_lst.append(notebook.cells)
+    cells_lst += notebook.cells
 
-# read meta data from a random notebook in notebook_lst
-target_notebook_dct['cells'] = cells_lst
-target_notebook_dct.update(notebook_lst[0].meta_data)
+# write target notebook
+target_notebook = Notebook()
+target_notebook.cells = cells_lst
+target_notebook.meta_data = notebook_lst[0].meta_data
 
-
-# convert to json string and export to target_notebook.ipynb
-target_str = json.dumps(target_notebook_dct)
 with open('target_notebook.ipynb', 'w') as target_file:
-    target_file.write(target_str)
+    target_file.write(target_notebook.json_str())
--- notebook.py
+++ notebook.py
@@ -1,8 +1,11 @@
 import json
 
 class Notebook:
-    def __init__(self, path):
-        self.cells, self.meta_data = self.read_notebook(path)
+    __slots__ = ('cells', 'meta_data')
+
+    def __init__(self, path=None):
+        if path is not None:
+            self.cells, self.meta_data = self.read_notebook(path)
 
     def read_notebook(self, path):
         notebook_dct = self.read_json(path)
@@ -17,3 +20,12 @@ class Notebook:
         with open(path) as notebook_file:
             s = notebook_file.read()
             return json.loads(s)
+
+    def json_str(self):
+        return json.dumps(self.json_dct(), indent=4)
+
+
+    def json_dct(self):
+        dct = { 'cells': self.cells }
+        dct.update(self.meta_data)
+        return dct
