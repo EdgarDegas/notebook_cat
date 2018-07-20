edd8636ee9fb8549c4786af4b8b4a15af3da0281
iMoeNya <sym9688@outlook.com>
2018-07-20 12:39:33 +0800

    Finish basic

--- cat.py
+++ cat.py
@@ -1,38 +1,35 @@
 import json
 import sys
 
+from notebook import Notebook
+
 # handle argument exceptions
 if len(sys.argv) < 3:
     raise Exception('参数数量必须大于 2！')
 
-
 # accept argument list
 notebook_path_lst = sys.argv[1:]
 
-target_notebook = {}
-cells_lst = []
+target_notebook_dct = {}
+
+# read notebooks
+notebook_lst = []
 
 # read notebook path list
 for path in notebook_path_lst:
-    with open(path) as notebook:
-        notebook_str = notebook.read()
-        notebook_json = json.loads(notebook_str)
+    notebook_lst.append(Notebook(path))
 
-        # read cells
-        cells = notebook_json['cells']
+# read cells from notebooks
+cells_lst = []
+for notebook in notebook_lst:
+    cells_lst.append(notebook.cells)
 
-        # take meta data from first notebook
-        if path is notebook_path_lst[0]:
-            del notebook_json['cells']
-            meta_data = notebook_json
+# read meta data from a random notebook in notebook_lst
+target_notebook_dct['cells'] = cells_lst
+target_notebook_dct.update(notebook_lst[0].meta_data)
 
-        # add cells into cells_lst
-        cells_lst += cells
 
-target_notebook['cells'] = cells_lst
-target_notebook.update(meta_data)
-
-target_str = json.dumps(target_notebook)
-
+# convert to json string and export to target_notebook.ipynb
+target_str = json.dumps(target_notebook_dct)
 with open('target_notebook.ipynb', 'w') as target_file:
     target_file.write(target_str)
