b5ea1ba095778c300183acf7ffa7a00795b1afb3
iMoeNya <sym9688@outlook.com>
2018-07-20 12:23:43 +0800

    finish

--- cat.py
+++ cat.py
@@ -17,28 +17,22 @@ for path in notebook_path_lst:
     with open(path) as notebook:
         notebook_str = notebook.read()
         notebook_json = json.loads(notebook_str)
+
+        # read cells
         cells = notebook_json['cells']
+
+        # take meta data from first notebook
+        if path is notebook_path_lst[0]:
+            del notebook_json['cells']
+            meta_data = notebook_json
+
+        # add cells into cells_lst
         cells_lst += cells
 
 target_notebook['cells'] = cells_lst
-
-
-
-
-
-del notebook1_json['cells']
-
-# cat two notebooks
-target_cells = cells1 + cells2
-
-
-
-# target_notebook['cells'] = target_cell
-target_notebook.update(notebook1_json)
+target_notebook.update(meta_data)
 
 target_str = json.dumps(target_notebook)
 
-
-
-target = open('target_notebook.ipynb', 'w')
-target.write(target_str)
+with open('target_notebook.ipynb', 'w') as target_file:
+    target_file.write(target_str)
